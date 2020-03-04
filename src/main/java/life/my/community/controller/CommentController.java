0fdea659ebda9644157a288a720ba52f293a801b
jackeras <964581542@qq.com>
2020-03-04 20:55:17 +0800

    add sql

---
+++ src/main/java/life/my/community/controller/CommentController.java
@@ -0,0 +1,13 @@
+package life.my.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CommentController {
+    @RequestMapping(value = "/comment",method = RequestMethod.POST)
+    public Object post(){
+
+    }
+}
--- src/main/resources/db/migration/V3__Create_question_table.sql
+++ src/main/resources/db/migration/V3__Create_question_table.sql
@@ -1,14 +1,13 @@
 create table question
 (
-	id int auto_increment primary key,
-	title VARCHAR(50),
-	desciption TEXT,
-	gmt_create BIGINT,
-	gmt_modified BIGINT,
-	creator int,
-	comment_count int default 0,
-	view_count int default 0,
-	like_count int default 0,
-	tag VARCHAR(256),
+    id int auto_increment primary key,
+    title varchar(50),
+    description text,
+    gmt_create bigint,
+    gmt_modified bigint,
+    creator int,
+    comment_count int default 0,
+    view_count int default 0,
+    like_count int default 0,
+    tag varchar(256)
 );
-
